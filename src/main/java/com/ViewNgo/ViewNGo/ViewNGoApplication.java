0e0a2d52e529a7add5f8ebc2d9a5c2949f9d3642
pranavb@SNPS-VmHJiMKHgO <pranavb@synopsys.com>
2025-07-19 21:44:24 +0530

    First stage of creating boiler templates for project(initial configs and strucutre)

---
+++ src/main/java/com/ViewNgo/ViewNGo/ViewNGoApplication.java
@@ -0,0 +1,13 @@
+package com.ViewNgo.ViewNGo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewNGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViewNGoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ViewNgo/ViewNGo/ViewNGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ViewNgo.ViewNGo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViewNGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
